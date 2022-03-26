fbf9c3d6083a629ffed6de599f2be58bfa47e681
Nikhil Verma 360 <nv5475609@bbdec.ac.in>
2022-03-26 15:47:25 +0530

    update game token

--- contracts/GameToken.sol
+++ contracts/GameToken.sol
@@ -6,14 +6,14 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract GameToken is ERC20, Ownable {
     constructor() ERC20("Game", "token") {
-        _mint(msg.sender, 1000 ether);
+        _mint(msg.sender, 10000 ether);
     }
 
     function faucet() public {
-        _mint(msg.sender, 1000 ether);
+        _mint(msg.sender, 10000 ether);
     }
 
     function mintTo(address _to) public {
-        _mint(_to, 1000 ether);
+        _mint(_to, 10000 ether);
     }
 }
